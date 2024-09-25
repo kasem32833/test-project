4822cee3e279502b0b3191a710b2687af82deb73
Kasem32833 <abulkasem32833@gmail.com>
2024-09-25 10:49:00 +0300

    add react-router-dom

--- src/App.jsx
+++ src/App.jsx
@@ -7,9 +7,7 @@ function App() {
 
   return (
     <>
-       <h1 className="text-3xl font-bold underline text-red-500">
-      Hello world!
-    </h1>
+        <p>Hello world</p>
    
     </>
   )
